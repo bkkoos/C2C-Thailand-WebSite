9fdd78e54fb6eb6dc9364e884421cbee6a9d7090
bkkoo <bk.koo.s@gmail.com>
2013-07-10 12:55:12 +0700

    Create Calm Index Page

--- src/main/scala/net/shantitree/calm/CalmScalatraServlet.scala
+++ src/main/scala/net/shantitree/calm/CalmScalatraServlet.scala
@@ -6,12 +6,9 @@ import scalate.ScalateSupport
 class CalmScalatraServlet extends CalmStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+
+    contentType="text/html"
+    jade("index", "title"->"Choose to be Calm", "headline"->"welcome")
   }
-  
+
 }
